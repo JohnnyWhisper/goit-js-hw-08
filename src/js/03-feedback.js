6f41b03f9335ae0617a9e5e6b054489a7504d9f3
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-05-10 23:27:30 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,8 +13,8 @@ function getFormData(e) {
 
 function onSubmitForm(e) {
   e.preventDefault();
-  if (!e.target.value) {
-  console.error("Поля повинні бути заповнені");
+  if (e.target.value = null) {
+  alert("Всі поля повинні бути заповнені");
   }
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
 
